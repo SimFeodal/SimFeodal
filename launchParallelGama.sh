088f7771b85114a34552ed5d50d10ab25f258a9a
Robin Cura <robin.cura@gmail.com>
2015-04-22 13:11:29 +0200

    Update from 12 to 8 simulations

--- launchParallelGama.sh
+++ launchParallelGama.sh
@@ -3,9 +3,13 @@ cd /home/robin/Gama/headless
 
 experimentPath="/home/robin/transition8/models/myExperiment.xml"
 outputPath="/home/robin/myOutputs/sim"
-for var in 0 2 4 6 8 10 12 14 16 18 20 22; do
+# Cluster got 24 cores
+# We launch each simulation on 3 cores, so, 8 simulations
+# Each with 8Go of RAM (64 Go Total)
+for var in 0 3 6 9 12 15 18 21; do
     var2=$(($var + 1))
-    echo taskset -c $var,$var2 sh gama-headless.sh -m 4096m $experimentPath $outputPath$var
-	taskset -c $var,$var2 sh gama-headless.sh -m 4096m $experimentPath $outputPath$var &
+    var3=$(($var + 2))
+    echo taskset -c $var,$var2,$var3 sh gama-headless.sh -m 8192m $experimentPath $outputPath$var
+	taskset -c $var,$var2,$var3 sh gama-headless.sh -m 8192m $experimentPath $outputPath$var &
 done 
 exit 0
