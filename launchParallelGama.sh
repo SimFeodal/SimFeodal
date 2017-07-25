a0ef76de4813075de9028787617f80019d5de3dd
Robin Cura <robin.cura@gmail.com>
2017-07-25 11:53:43 +0200

    change batch mode params

--- launchParallelGama.sh
+++ launchParallelGama.sh
@@ -4,12 +4,14 @@ cd /home/robin/Gama/headless
 experimentPath="/home/robin/transition8/models/myExperiment.xml"
 outputPath="/home/robin/myOutputs/sim"
 # Cluster got 24 cores
-# We launch each simulation on 3 cores, so, 8 simulations
-# Each with 8Go of RAM (64 Go Total)
-for var in 0 3 6 9 12 15 18 21; do
+# We launch each simulation on 2 cores, so, 8 simulations
+# Each with 4Go of RAM (64 Go Total)
+# Server
+# for var in 0 2 4 6 8 10 12 14 16 18; do
+# Local
+for var in 0 2; do
     var2=$(($var + 1))
-    var3=$(($var + 2))
-    echo taskset -c $var,$var2,$var3 sh gama-headless.sh -m 8192m $experimentPath $outputPath$var
-	taskset -c $var,$var2,$var3 sh gama-headless.sh -m 8192m $experimentPath $outputPath$var &
+    echo taskset -c $var,$var2 sh gama-headless.sh -m 4096m $experimentPath $outputPath$var
+	taskset -c $var,$var2 sh gama-headless.sh -m 4096m $experimentPath $outputPath$var &
 done 
 exit 0
