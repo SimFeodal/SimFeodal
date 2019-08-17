516c895c2c6576ebcb0a2aabed56545213479007
Robin Cura <robin.cura@gmail.com>
2019-08-17 16:42:10 +0200

    test2

--- batch_run/launchParallelGama.sh
+++ batch_run/launchParallelGama.sh
@@ -11,7 +11,7 @@ for var in 0 2 4 6 8 10 12 14 16 18; do
 # Local
 #for var in 0 2; do
     var2=$(($var + 1))
-    echo taskset -c $var,$var2 bash gama-headless.sh -m 2048m -hpc 2 $experimentPath $outputPath$var
-	taskset -c $var,$var2 bash gama-headless.sh -m 2048m -hpc 2 $experimentPath $outputPath$var &
+    echo taskset -c $var,$var2 bash gama-headless.sh -m 4096m $experimentPath $outputPath$var
+	taskset -c $var,$var2 bash gama-headless.sh -m 4096m $experimentPath $outputPath$var &
 done 
 exit 0
