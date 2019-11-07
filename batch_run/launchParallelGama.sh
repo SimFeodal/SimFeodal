d98cf7b85745a4b390ede43ecabfbf3daac6e230
Robin Cura <robin.cura@gmail.com>
2019-11-07 12:12:12 +0100

    clean

--- batch_run/launchParallelGama.sh
+++ batch_run/launchParallelGama.sh
@@ -6,11 +6,11 @@ outputPath="/home/robin/myOutputs/sim"
 # We launch each simulation on 2 cores, so, 10 simulations
 # Each with 4Go of RAM (64 Go Total)
 # Server
-for var in 0 2 4 6 8 10 12 14 16 18; do
+#for var in 0 2 4 6 8 10 12 14 16 18; do
 # Local
-# for var in 0 2; do
+for var in 0 2; do
     var2=$(($var + 1))
-    echo taskset -c $var,$var2 bash gama-headless.sh -m 8000m -hpc 2 $experimentPath $outputPath$var
-	taskset -c $var,$var2 bash gama-headless.sh -m 8000m -hpc 2 $experimentPath $outputPath$var &
+    echo taskset -c $var,$var2 bash gama-headless.sh -m 2500m -hpc 2 $experimentPath $outputPath$var
+	taskset -c $var,$var2 bash gama-headless.sh -m 2500m -hpc 2 $experimentPath $outputPath$var &
 done 
 exit 0
